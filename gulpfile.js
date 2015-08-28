e34df75530e3120b89320804ccb761e3de67d27e
inuscript <inuscript@gmail.com>
2015-08-28 09:42:06 +0900

    gulp

---
+++ gulpfile.js
@@ -0,0 +1,55 @@
+
+var gulp = require('gulp');
+var path = require("path")
+var browserify = require('browserify');
+var babelify = require('babelify');
+var source = require('vinyl-source-stream');
+var glob = require("glob")
+var del = require("del")
+var gutil = require("gutil")
+var replaceExt = require('replace-ext')
+var dest = './javascript/'
+
+
+gulp.task('clean', function() {
+  del(dest)
+})
+var libs = [
+  "react", "redux"
+]
+// https://github.com/greypants/gulp-starter/issues/75
+gulp.task('browserify-lib', function() {
+  var b = browserify({
+    require: libs
+  })
+  b.bundle()
+    .pipe(source("lib.js"))
+    .pipe(gulp.dest(path.join(dest, "lib")))
+})
+
+gulp.task('browserify-client', function() {
+  var base = "./src/entry/"
+  var entries = "**/*"
+  glob.sync(entries, {cwd: base}).forEach(function(file){
+    var b = browserify({
+      entries: path.join(base, file),
+      extensions: ['jsx', 'js'],
+      transform: [babelify],
+      externals: libs
+    })
+    libs.forEach(function(lib){
+      b.external(lib)
+    })
+    var fileName = replaceExt(file, ".js")
+    b.bundle()
+      .pipe(source(fileName))
+      .pipe(gulp.dest(path.join(dest, "client")))
+  })
+});
+
+gulp.task('watch', function() {
+  gulp.watch('js/**/*', ['browserify'])
+});
+
+gulp.task('browserify', [ 'browserify-lib', 'browserify-client']);
+gulp.task('default', ['browserify', 'watch']);
---
+++ src/component/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { createStore } from 'redux';
+
+class App extends React.Component{
+  render(){
+    return <div></div>
+  }
+}
