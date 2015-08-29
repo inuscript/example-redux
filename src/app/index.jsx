378257f05283291e13ba10f385dd0b2802cd58c6
inuscript <inuscript@gmail.com>
2015-08-29 22:37:33 +0900

    module fix

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import App from "./component/App.jsx"
+
+export default function startApp(){
   var container = document.querySelector("body")
   React.render(<App />, container)
 }
