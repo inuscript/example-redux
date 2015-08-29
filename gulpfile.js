a37937bd1dab04c2fce5567015f1d8da2bd9e3e8
inuscript <inuscript@gmail.com>
2015-08-29 22:13:02 +0900

    add

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,3 @@
-
 var gulp = require('gulp');
 var path = require("path")
 var browserify = require('browserify');
@@ -7,16 +6,26 @@ var source = require('vinyl-source-stream');
 var glob = require("glob")
 var del = require("del")
 var gutil = require("gutil")
+var factor = require('factor-bundle');
 var replaceExt = require('replace-ext')
+
 var dest = './javascript/'
 
 
 gulp.task('clean', function() {
   del(dest)
 })
+
 var libs = [
   "react", "redux"
 ]
+
+var externalLibs = function(b){
+  libs.forEach(function(lib){
+    b.external(lib)
+  })
+  return b
+}
 // https://github.com/greypants/gulp-starter/issues/75
 gulp.task('browserify-lib', function() {
   var b = browserify({
@@ -25,19 +34,35 @@ gulp.task('browserify-lib', function() {
     .pipe(source("lib.js"))
     .pipe(gulp.dest(path.join(dest, "lib")))
 })
+// 
+// gulp.task('browserify-app', function() {
+//   var apps = glob.sync("./src/app/**/*", {nodir: true })
+//   var b = browserify({
+//     extensions: ['js', 'jsx'],
+//   })
+//   b.require("./src/app/")
+//   // apps.forEach(function(a){
+//   //   b.require(a)
+//   // })
+//   b = externalLibs(b)  
+//   b.transform(babelify)
+//   b.bundle()
+//     .pipe(source("app.js"))
+//     .pipe(gulp.dest(path.join(dest, "lib")))
+// })
 
-gulp.task('browserify-client', function() {
+gulp.task('browserify-entry', function() {
   var base = "./src/entry/"
   var entries = "**/*"
   glob.sync(entries, {cwd: base}).forEach(function(file){
     var b = browserify({
       entries: path.join(base, file),
-      extensions: ['jsx', 'js'],
-      transform: [babelify],
+      extensions: ['js', 'jsx'],
+    }).plugin(factor, {
+      outputs: glob.sync(entries)
     })
-    libs.forEach(function(lib){
-      b.external(lib)
-    })
+    b = externalLibs(b)
+    b.transform(babelify)
     var fileName = replaceExt(file, ".js")
     b.bundle()
       .pipe(source(fileName))
@@ -49,5 +74,9 @@ gulp.task('watch', function() {
   gulp.watch('src/**/*', ['browserify'])
 });
 
-gulp.task('browserify', [ 'browserify-lib', 'browserify-client']);
+gulp.task('browserify', [
+  'browserify-lib',
+  // 'browserify-app',
+  'browserify-entry'
+]);
 gulp.task('default', ['browserify', 'watch']);
--- src/component/App.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import { createStore } from 'redux';
-
-export default class App extends React.Component{
-  render(){
-    return <div>Hello</div>
-  }
-}
