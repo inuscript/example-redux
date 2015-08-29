7e3f0948dd9612c963206eb994d27a3100540032
inuscript <inuscript@gmail.com>
2015-08-29 22:16:12 +0900

    fix

---
+++ src/app/component/App.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import { createStore } from 'redux';
+
+export default class App extends React.Component{
+  render(){
+    return <div>Hello</div>
+  }
+}
---
+++ src/entry/index.js
@@ -0,0 +1,7 @@
+import startApp from "../app/index.jsx"
+
+export default function(){
+  document.addEventListener('DOMContentLoaded', function(){
+    startApp()
+  })
+}
